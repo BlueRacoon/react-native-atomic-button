88a70a07730ee24c62409b66e3e93d70a19f0958
Zachary Owen <zowen@Zacharys-MBP.attlocal.net>
2025-02-08 23:22:03 -0500

    Fix: Update AtomicButtonManager to use moduleForName for UIManager

--- ios/AtomicButton.h
+++ ios/AtomicButton.h
@@ -1,11 +1,10 @@
 #import <UIKit/UIKit.h>
-
-// Optionally, if you need React types, import them too:
-// #import <React/RCTView.h>
+#import <React/RCTComponent.h> // Import for RCTBubblingEventBlock
 
 @interface AtomicButton : UIButton
 
 @property (nonatomic, assign) BOOL isDisabled;
+@property (nonatomic, copy) RCTBubblingEventBlock onPress; // Callback for press events
 
 - (void)reset;
 
--- ios/AtomicButton.m
+++ ios/AtomicButton.m
@@ -1,27 +1,40 @@
-// File: ios/AtomicButton.m
 #import "AtomicButton.h"
 
 @implementation AtomicButton
 
 - (instancetype)initWithFrame:(CGRect)frame {
-    if (self = [super initWithFrame:frame]) {
-        self.isDisabled = NO;
-        [self addTarget:self action:@selector(handlePress) forControlEvents:UIControlEventTouchUpInside];
-    }
-    return self;
+  if (self = [super initWithFrame:frame]) {
+    self.isDisabled = NO;
+    // Ensure the button is enabled for user interaction initially.
+    self.userInteractionEnabled = YES;
+    [self addTarget:self action:@selector(handlePress) forControlEvents:UIControlEventTouchUpInside];
+  }
+  return self;
 }
 
 - (void)handlePress {
-    if (!self.isDisabled) {
-        self.isDisabled = YES;
-        self.alpha = 0.5; // Visual feedback
-        // Send a control event that the manager can observe
-        [self sendActionsForControlEvents:UIControlEventPrimaryActionTriggered];
-    }
+  // If already disabled, do nothing.
+  if (self.isDisabled) {
+    return;
+  }
+  
+  // Immediately disable the button and provide visual feedback.
+  self.isDisabled = YES;
+  self.userInteractionEnabled = NO;
+  self.alpha = 0.5; // Change appearance instantly
+  
+  // Call the onPress callback if provided.
+  if (self.onPress) {
+    // You can pass an empty dictionary or any event details.
+    self.onPress(@{});
+  }
 }
 
 - (void)reset {
-    self.isDisabled = NO;
-    self.alpha = 1.0;
+  // Reset the button so that it can be pressed again.
+  self.isDisabled = NO;
+  self.userInteractionEnabled = YES;
+  self.alpha = 1.0;
 }
+
 @end
